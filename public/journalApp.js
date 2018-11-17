c9bbf5233bf009f697c71bb0c57ce09d3eb7268c
jmutt78 <jmcintosh@docusites.com>
2018-11-17 15:25:27 -0800

    Polished the journal feature

--- public/journalApp.js
+++ public/journalApp.js
@@ -3,11 +3,9 @@ var QUESTION1 = {
     "id": "11111",
     "publishedDate": 1470011976609,
     "name": "3 things they are thankful for",
-    "threeThankful": {
-      "text1": "life",
-      "text2": "wife",
-      "text3": "dog"
-    }
+    "text1": "life",
+    "text2": "wife",
+    "text3": "dog"
   }]
 };
 var QUESTION2 = {
@@ -15,11 +13,9 @@ var QUESTION2 = {
     "id": "222222",
     "publishedDate": 1470011976609,
     "name": "3 things that will make today great",
-    "threeGreat": {
-      "text1": "finsh this code",
-      "text2": "pary",
-      "text3": "live"
-    }
+    "text1": "finsh this code",
+    "text2": "pary",
+    "text3": "live"
   }]
 };
 var QUESTION3 = {
@@ -49,7 +45,15 @@ function displayJournal(data) {
   //Stretch Goasl
   for (index in data.question1) {
     $('.display-journal').append(
-      '<p>' + data.question1[index].name  + '</p>');
+      '<p>' + data.question1[index].name  + '<br>' + data.question1[index].text1 + '<br>' + data.question1[index].text2 + '<br>' + data.question1[index].text3 + '</p>');
+}
+for (index in data.question2) {
+  $('.display-journal').append(
+    '<p>' + data.question2[index].name  + '<br>' + data.question2[index].text1 + '<br>' + data.question2[index].text2 + '<br>' + data.question2[index].text3 + '</p>');
+}
+for (index in data.question3) {
+ $('.display-journal').append(
+    '<p>' + data.question3[index].name + '<br>' + data.question3[index].text + '</p>');
 }
 };
 
