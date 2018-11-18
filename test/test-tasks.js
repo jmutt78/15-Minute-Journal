5dc40f612b2f89010bf3d4644a6c050e8dc148d0
jmutt78 <jmcintosh@docusites.com>
2018-11-18 08:17:08 -0800

    First test for server and get requests

--- test/test-tasks.js
+++ test/test-tasks.js
@@ -1,113 +1,91 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+'use strict';
 
-const expect = chai.expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
-const { app, runServer, closeServer } = require("../server");
+
+const should = chai.should();
+
+const {
+  Tasks
+} = require('../models');
+const {
+  closeServer,
+  runServer,
+  app
+} = require('../server');
+const {
+  TEST_DATABASE_URL
+} = require('../config');
 
 chai.use(chaiHttp);
 
-describe("Tasks", function() {
+
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+
+
+function seedTaskData() {
+  console.info('seeding task data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      text: faker.lorem.sentence(),
+      due: faker.date.future(2)
+    });
+  }
+  // this will return a promise
+  return Tasks.insertMany(seedData);
+}
+
+
+describe('task API resource', function() {
+
   before(function() {
-    return runServer();
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedTaskData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
   });
 
   after(function() {
     return closeServer();
   });
 
-//GET
-  it("should list items on GET", function() {
-    return chai
-      .request(app)
-      .get("/tasks")
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a("array");
-        expect(res.body.length).to.be.above(0);
-        res.body.forEach(function(item) {
-          expect(item).to.be.a("object");
-          expect(item).to.have.all.keys(
-            "id",
-            "text",
-            "created",
-            "due"
-          );
+  describe('GET endpoint', function() {
+
+    it('should return all existing tasks', function() {
+
+      let res;
+      return chai.request(app)
+        .get('/tasks')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.lengthOf.at.least(1);
+
+          return Tasks.count();
+        })
+        .then(count => {
+
+          res.body.should.have.lengthOf(count);
         });
-      });
-  });
+    });
 
-//POST
-  it("should add a task on POST", function() {
-    const newTask = {
-      text: "Lorem ip some",
-      due: 43388
-    };
-    const expectedKeys = ["id", "created"].concat(Object.keys(newTask));
-
-    return chai
-      .request(app)
-      .post("/tasks")
-      .send(newPost)
-      .then(function(res) {
-        expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a("object");
-        expect(res.body).to.have.all.keys(expectedKeys);
-        expect(res.body.title).to.equal(newTask.title);
-        expect(res.body.content).to.equal(newTask.content);
-        expect(res.body.author).to.equal(newTask.author);
-      });
-  });
-
-  it("should error if POST missing expected values", function() {
-    const badRequestData = {};
-    return chai
-      .request(app)
-      .post("/tasks")
-      .send(badRequestData)
-      .then(function(res) {
-        expect(res).to.have.status(400);
-      });
-  });
-//PUT
-  it("should update blog posts on PUT", function() {
-    return (
-      chai
-        .request(app)
-        // first have to get
-        .get("/tasks")
-        .then(function(res) {
-          const updatedTask = Object.assign(res.body[0], {
-            text: "connect the dots",
-            due: 43389
-          });
-          return chai
-            .request(app)
-            .put(`/tasks/${res.body[0].id}`)
-            .send(updatedTask)
-            .then(function(res) {
-              expect(res).to.have.status(204);
-            });
-        })
-    );
-  });
-//DELETE
-  it("should delete task on DELETE", function() {
-    return (
-      chai
-        .request(app)
-        // first have to get
-        .get("/tasks")
-        .then(function(res) {
-          return chai
-            .request(app)
-            .delete(`/tasks/${res.body[0].id}`)
-            .then(function(res) {
-              expect(res).to.have.status(204);
-            });
-        })
-    );
   });
 });
