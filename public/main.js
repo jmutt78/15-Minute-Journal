24884ea4f40820119f99990e56e91b01a298eacd
jmutt78 <jmcintosh@docusites.com>
2018-11-26 07:58:05 -0800

    Ajax call

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 'use strict';
+
+
 $(document).ready(function() {
   console.log("ready!");
 });
@@ -31,17 +33,70 @@ function handleNavBar() {
 //-------------Goals-------------------//
 //get request to the api
 function getGoalsData(){
+$('#goals-nav').click(function() {
+const stretchGoalUrl = 'https://obscure-ocean-89688.herokuapp.com/stretch'
+$.ajax({
+  type: 'GET',
+  url: stretchGoalUrl,
+  datatype: 'jsonp',
+  success: function (data){
+    console.log('success', data);
+  }
 
+});
 
+});
 }
 
 
 //Checks to see if thier is an object and if there is if its completed
 function fetchResults(){
 
+
+
 }
 
+//display anyy goals that are active
+function displayActiveGoals(){
 
+
+}
+
+//display goal forms if no object exists
+function displayGoalForms(){
+
+
+}
+// takes info form user and makes a post
+function pushGoalForm(){
+
+
+}
+
+//delete goals
+function deleteGoals(){
+
+
+}
+
+//complete goals
+function completeGoals(){
+
+
+}
+
+//update Goals
+
+function updateGoals(){
+
+
+}
+
+//handle Goals
+function handleGoals(){
+
+
+}
 
 
 //-------------Tasks-------------------//
@@ -62,6 +117,7 @@ function handleJournal() {
   $('.goals-page').hide();
   $('.task-page').hide();
   $('.daily-page').hide();
+  getGoalsData();
   handleNavBar();
 }
 
