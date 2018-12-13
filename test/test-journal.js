220efab2a8a576628a16cbdcfd64a1743c8862b7
jmutt78 <jmcintosh@docusites.com>
2018-12-13 14:31:03 -0800

    working through testing

--- test/test-journal.js
+++ test/test-journal.js
@@ -1,11 +1,12 @@
 'use strict';
-
+require('dotenv').config();
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
-
-
+const jwt = require('jsonwebtoken');
+const {User} = require('../users');
+const {JWT_SECRET} = require('../config');
 const should = chai.should();
 
 const {
@@ -51,17 +52,32 @@ function seedDailyData() {
 
 //Server
 describe('daily API resource', function() {
+  const username = 'exampleUser';
+  const password = 'examplePass';
+  const firstName = 'Example';
+  const lastName = 'User';
 
   before(function() {
     return runServer(TEST_DATABASE_URL);
   });
 
   beforeEach(function() {
-    return seedDailyData();
+    return
+    User.hashPassword(password).then(password =>
+      User.create({
+        username,
+        password,
+        firstName,
+        lastName
+      })
+    );
+    seedDailyData();
   });
 
   afterEach(function() {
-    return tearDownDb();
+    return
+    User.remove({});
+    tearDownDb();
   });
 
   after(function() {
@@ -72,10 +88,110 @@ describe('daily API resource', function() {
 describe('GET endpoint', function () {
 
    it('should return all existing daily', function () {
+     it('Should reject requests with no credentials', function() {
+       return chai
+         .request(app)
+         .get('/daily')
+         .then(() =>
+           expect.fail(null, null, 'Request should not succeed')
+         )
+         .catch(err => {
+           if (err instanceof chai.AssertionError) {
+             throw err;
+           }
+
+           const res = err.response;
+           expect(res).to.have.status(401);
+         });
+     });
+
+     it('Should reject requests with an invalid token', function() {
+       const token = jwt.sign(
+         {
+           username,
+           firstName,
+           lastName
+         },
+         'wrongSecret',
+         {
+           algorithm: 'HS256',
+           expiresIn: '7d'
+         }
+       );
+
+       return chai
+         .request(app)
+         .get('/daily')
+         .set('Authorization', `Bearer ${token}`)
+         .then(() =>
+           expect.fail(null, null, 'Request should not succeed')
+         )
+         .catch(err => {
+           if (err instanceof chai.AssertionError) {
+             throw err;
+           }
+
+           const res = err.response;
+           expect(res).to.have.status(401);
+         });
+     });
+
+     it('Should reject requests with an expired token', function() {
+       const token = jwt.sign(
+         {
+           user: {
+             username,
+             firstName,
+             lastName
+           },
+           exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+         },
+         JWT_SECRET,
+         {
+           algorithm: 'HS256',
+           subject: username
+         }
+       );
+
+       return chai
+         .request(app)
+         .get('/daily')
+         .set('authorization', `Bearer ${token}`)
+         .then(() =>
+           expect.fail(null, null, 'Request should not succeed')
+         )
+         .catch(err => {
+           if (err instanceof chai.AssertionError) {
+             throw err;
+           }
+
+           const res = err.response;
+           expect(res).to.have.status(401);
+         });
+     });
+
+
 
      let res;
-     return chai.request(app)
+     const token = jwt.sign(
+        {
+          user: {
+            username,
+            firstName,
+            lastName
+          }
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: username,
+          expiresIn: '7d'
+        }
+      );
+     return chai
+       .request(app)
        .get('/daily')
+       .set('authorization', `Bearer ${token}`)
        .then(_res => {
          res = _res;
          res.should.have.status(200);
